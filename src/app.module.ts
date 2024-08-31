ab749fc4cc6dc1eab0bdea1bdcaf1dd89cdff116
luccaparadeda <luccaparadedaprofessional@gmail.com>
2024-08-31 10:12:19 -0300

    feat: chat module remove

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { ChatModule } from "./chat/chat.module";
 import { CategoryModule } from "./category/category.module";
 import { FileModule } from "./file/file.module";
 import { AuthModule } from "./auth/auth.module";
